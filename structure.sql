069d43cb78fbef9a7ebbf6532e8613b9b3828e8b
yaboariel <61515878+yaboariel@users.noreply.github.com>
2021-04-19 11:35:39 -0300

    Update structure.sql

--- structure.sql
+++ structure.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `Product` (
+CREATE TABLE `products` (
    `id` INT AUTO_INCREMENT,
    `title` VARCHAR(255),
    `model` VARCHAR(255),
@@ -14,7 +14,7 @@ CREATE TABLE `Product` (
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Size` (
+CREATE TABLE `sizes` (
    `id` INT,
    `number` int,
    PRIMARY KEY (`id`)
@@ -43,13 +43,13 @@ CREATE TABLE `Prod_cart` (
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Color` (
+CREATE TABLE `colors` (
    `id` INT AUTO_INCREMENT,
    `name` VARCHAR(255),
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Cart` (
+CREATE TABLE `carts` (
    `id` INT,
    `prodid` INT,
    `qty` int,
@@ -58,25 +58,25 @@ CREATE TABLE `Cart` (
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Brand` (
+CREATE TABLE `brands` (
    `id` INT,
    `nombre` VARCHAR(255),
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Material` (
+CREATE TABLE `materials` (
    `id` INT AUTO_INCREMENT,
    `nombre` INT AUTO_INCREMENT,
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Category` (
+CREATE TABLE `categories` (
    `id` INT AUTO_INCREMENT,
    `nombre` VARCHAR(255),
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `User` (
+CREATE TABLE `users` (
    `id` INT,
    `nombre` VARCHAR(255),
    `apellido` VARCHAR(255),
@@ -90,33 +90,33 @@ CREATE TABLE `User` (
    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Foto` (
+CREATE TABLE `fotos` (
    `id` INT AUTO_INCREMENT,
    `filename` VARCHAR(255),
    PRIMARY KEY (`id`)
 );
 
 
-ALTER TABLE `Product` ADD CONSTRAINT `FK_4d58f8d8-c6e7-4ca9-8534-002331756e14` FOREIGN KEY (`brand_id`) REFERENCES `Brand`(`id`);
+ALTER TABLE `products` ADD CONSTRAINT `FK_4d58f8d8-c6e7-4ca9-8534-002331756e14` FOREIGN KEY (`brand_id`) REFERENCES `brands`(`id`);
 
-ALTER TABLE `Product` ADD CONSTRAINT `FK_fa67bf4c-1d5e-4a68-b60c-f3bc55fcb517` FOREIGN KEY (`material_id`) REFERENCES `Material`(`id`);
+ALTER TABLE `products` ADD CONSTRAINT `FK_fa67bf4c-1d5e-4a68-b60c-f3bc55fcb517` FOREIGN KEY (`material_id`) REFERENCES `materials`(`id`);
 
-ALTER TABLE `Product` ADD CONSTRAINT `FK_e2a3accb-cd36-4478-ac31-93935d8d4901` FOREIGN KEY (`category_id`) REFERENCES `Category`(`id`);
+ALTER TABLE `products` ADD CONSTRAINT `FK_e2a3accb-cd36-4478-ac31-93935d8d4901` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`);
 
-ALTER TABLE `Prod_size` ADD CONSTRAINT `FK_3882a1be-3718-4ca2-adef-4a6643f6b9e2` FOREIGN KEY (`prod_id`) REFERENCES `Product`(`id`);
+ALTER TABLE `Prod_size` ADD CONSTRAINT `FK_3882a1be-3718-4ca2-adef-4a6643f6b9e2` FOREIGN KEY (`prod_id`) REFERENCES `products`(`id`);
 
-ALTER TABLE `Prod_size` ADD CONSTRAINT `FK_11175114-bd12-439e-8693-6ee2b8fbb013` FOREIGN KEY (`size_id`) REFERENCES `Size`(`id`);
+ALTER TABLE `Prod_size` ADD CONSTRAINT `FK_11175114-bd12-439e-8693-6ee2b8fbb013` FOREIGN KEY (`size_id`) REFERENCES `sizes`(`id`);
 
-ALTER TABLE `Prod_color` ADD CONSTRAINT `FK_2abd7455-6d34-4347-988b-37cc69341486` FOREIGN KEY (`color_id`) REFERENCES `Color`(`id`);
+ALTER TABLE `Prod_color` ADD CONSTRAINT `FK_2abd7455-6d34-4347-988b-37cc69341486` FOREIGN KEY (`color_id`) REFERENCES `colors`(`id`);
 
-ALTER TABLE `Prod_cart` ADD CONSTRAINT `FK_90d51bec-9ddc-4ec1-abba-05882b3077e0` FOREIGN KEY (`prod_id`) REFERENCES `Product`(`id`);
+ALTER TABLE `Prod_cart` ADD CONSTRAINT `FK_90d51bec-9ddc-4ec1-abba-05882b3077e0` FOREIGN KEY (`prod_id`) REFERENCES `products`(`id`);
 
-ALTER TABLE `Prod_cart` ADD CONSTRAINT `FK_d71abdaa-9b8a-4496-97ce-13b7ca5121d1` FOREIGN KEY (`cart_id`) REFERENCES `Cart`(`id`);
+ALTER TABLE `Prod_cart` ADD CONSTRAINT `FK_d71abdaa-9b8a-4496-97ce-13b7ca5121d1` FOREIGN KEY (`cart_id`) REFERENCES `carts`(`id`);
 
-ALTER TABLE `Cart` ADD CONSTRAINT `FK_c09d1337-e02e-4234-a00c-31c0a5d2876f` FOREIGN KEY (`prodid`) REFERENCES `Product`(`id`);
+ALTER TABLE `carts` ADD CONSTRAINT `FK_c09d1337-e02e-4234-a00c-31c0a5d2876f` FOREIGN KEY (`prodid`) REFERENCES `products`(`id`);
 
-ALTER TABLE `Cart` ADD CONSTRAINT `FK_f3805bb3-334a-4d59-98a1-1150017c4b00` FOREIGN KEY (`size_id`) REFERENCES `Size`(`id`);
+ALTER TABLE `carts` ADD CONSTRAINT `FK_f3805bb3-334a-4d59-98a1-1150017c4b00` FOREIGN KEY (`size_id`) REFERENCES `sizes`(`id`);
 
-ALTER TABLE `Cart` ADD CONSTRAINT `FK_f310d5e8-89c5-4f29-b80d-0fa6707c4b93` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`);
+ALTER TABLE `carts` ADD CONSTRAINT `FK_f310d5e8-89c5-4f29-b80d-0fa6707c4b93` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
 
-ALTER TABLE `User` ADD CONSTRAINT `FK_1f7b1bf0-d8e2-447d-a632-bd50c27f9da4` FOREIGN KEY (`cart_id`) REFERENCES `Cart`(`id`);
+ALTER TABLE `users` ADD CONSTRAINT `FK_1f7b1bf0-d8e2-447d-a632-bd50c27f9da4` FOREIGN KEY (`cart_id`) REFERENCES `carts`(`id`);
